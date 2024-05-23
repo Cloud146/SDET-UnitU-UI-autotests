7e3aed815342ab7937d43afd574f341d1f728567
Alex_Seburev <cloud2886@gmail.com>
2024-05-23 15:39:10 +0300

    UI5 - Cookies - commit_2

--- src/test/java/Helpers/CookieUtils.java
+++ src/test/java/Helpers/CookieUtils.java
@@ -8,14 +8,25 @@ import java.io.FileWriter;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import io.qameta.allure.Step;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 
-
+/**
+ * Вспомогательный класс с методами работы с cookies
+ * @author Alex Seburev
+ */
 public class CookieUtils {
+
+    /**
+     * Функция сохранения cookies со страницы в файл json
+     * @param driver - объект вебдрайвера
+     * @param filePath - путь сохранения файла
+     * */
+    @Step("Сохранение cookie в файл json")
     public static void saveCookiesToFile(WebDriver driver, String filePath) throws IOException {
         Set<Cookie> cookies = driver.manage().getCookies();
 
@@ -27,6 +38,12 @@ public class CookieUtils {
         }
     }
 
+    /**
+     * Функция чтения cookies из файла json
+     * @param driver - объект вебдрайвера
+     * @param filePath - путь к сохранённому файлу
+     * */
+    @Step("Чтение cookie из файла json")
     public static void loadCookiesFromFile(WebDriver driver, String filePath) throws IOException {
         Type type = new TypeToken<HashSet<Cookie>>() {}.getType();
         Set<Cookie> cookiesFromFile = new Gson().fromJson(new FileReader(filePath), type);
