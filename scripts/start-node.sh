59e521f4c77070c398df7331caab2f9b2e0598cf
Alex_Seburev <cloud2886@gmail.com>
2024-05-27 13:37:18 +0300

    UI7 - ParallelTesting - commit_1

---
+++ scripts/start-node.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar selenium-server-4.21.0.jar node -hub http://localhost:4444/grid/register
--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -1,5 +1,6 @@
 package Tests;
 
+
 import Helpers.ConfigurationProvider;
 import Helpers.OutputData;
 import Helpers.PageActions;
@@ -9,11 +10,14 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -30,13 +34,17 @@ public class BaseTest {
     @Description("Открытие браузера с соответствующими настройками")
     @BeforeMethod
     public void browserSetUp() throws IOException {
-        driver = new ChromeDriver(new ChromeOptions()
-                .addArguments("--remote-allow-origins=**")
-                .addArguments("--disable-gpu")
-                .addArguments("--disable-infobars")
-                .addArguments("--start-maximized"));
-        driver.manage().window().setSize(new Dimension(configurationProvider.getScreenWidth(), configurationProvider.getScreenHeight()));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        driver = new RemoteWebDriver(new URL("http://172.30.112.1:4444/wd/hub"), capabilities);
+
+//        driver = new ChromeDriver(new ChromeOptions()
+//                .addArguments("--remote-allow-origins=**")
+//                .addArguments("--disable-gpu")
+//                .addArguments("--disable-infobars")
+//                .addArguments("--start-maximized"));
+//        driver.manage().window().setSize(new Dimension(configurationProvider.getScreenWidth(), configurationProvider.getScreenHeight()));
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
 
